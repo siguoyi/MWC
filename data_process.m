eab805a5e917879340158c53939dce70773f47cd
antcl <believemine@163.com>
2016-06-05 10:48:52 +0800

    change the Y axis of figure

--- data_process.m
+++ data_process.m
@@ -5,8 +5,9 @@ ber = xlsread('ber.xlsx');
 bb = ones(9,1);
 meanBer = mean(ber,2);
 meanBer = bb - meanBer/64;
-plot(meanBer, '-o','linewidth',2)
+semilogy(meanBer, '-o','linewidth',2)
 set(gca,'xticklabel',get(gca,'xTick')*5-15);
-xlabel('Input SNR');
+% grid on
+xlabel('Sampling SNR');
 ylabel('BER');
 
